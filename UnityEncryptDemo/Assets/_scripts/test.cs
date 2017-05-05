3d7df45874c52fe365fa78a0d103679eca5eef0d
yuliang <109827298@qq.com>
2017-05-05 16:07:35 +0800

    add

---
+++ UnityEncryptDemo/Assets/_scripts/test.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+
+    public static int A = 10;
+
+    public Text m_Show;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void ShowValue()
+    {
+        A++;
+        m_Show.text = A.ToString();
+    }
+}
