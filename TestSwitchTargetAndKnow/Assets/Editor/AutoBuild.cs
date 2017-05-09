9d9c3a0dc92c1f29a407d320ecb290188f894947
yuliang <109827298@qq.com>
2017-05-09 11:14:15 +0800

    add file

---
+++ TestSwitchTargetAndKnow/Assets/Editor/AutoBuild.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+
+public class AutoBuild
+{
+    [MenuItem("Tools/ActiveBuildTarget")]
+    public static void ActiveBuildTarget()
+    {
+        EditorUserBuildSettings.activeBuildTargetChanged = delegate() {
+			if(EditorUserBuildSettings.activeBuildTarget == BuildTarget.Android){
+				Debug.Log("Switch to android Done!");
+                Console.WriteLine("Switch to android Done!");
+			}
+		};
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+    }
+}
