a622388bfcec49f26f7e2a24a7bd20fbdd160ca7
yuliang <109827298@qq.com>
2017-10-23 19:02:51 +0800

    add files

---
+++ TestOldAB/Assets/Editor/TestPack.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TestPack
+{
+
+    [MenuItem("AB Editor/Create AssetBundles ALL")]
+    static void CreateAssetBundlesALL()
+    {
+        Caching.CleanCache();
+
+        string strPath = Application.dataPath + "/StreamingAssets/ALL.assetbundle";
+
+
+        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+        foreach (Object obj in SelectedAsset)
+        {
+            Debug.Log("Create AssetBundles name :" + obj);
+        }
+
+        if (BuildPipeline.BuildAssetBundle(null, SelectedAsset, strPath, BuildAssetBundleOptions.CollectDependencies))
+        {
+            AssetDatabase.Refresh();
+        }
+    }
+
+
+    [MenuItem("AB Editor/Create AssetBundles Main")]
+    static void CreateAssetbundlesMain()
+    {
+        //Debug.Log("CreateAssetbundlesMain");
+
+        Object[] SelectedAsset = Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets);
+
+
+        foreach (Object obj in SelectedAsset)
+        {
+            string sourcePath = AssetDatabase.GetAssetPath(obj);
+
+            //Debug.Log(obj.name);
+
+            //if ((obj.name == "Cube1") || (obj.name == "Cube2"))
+            //{
+            //    string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+
+            //    if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies))
+            //    {
+            //        Debug.Log(obj.name + " 资源打包成功");
+            //    }
+            //    else
+            //    {
+            //        Debug.Log(obj.name + " 资源打包失败");
+            //    }
+            //}
+
+            string targetPath = Application.dataPath + "/StreamingAssets/" + obj.name + ".assetbundle";
+
+            if (BuildPipeline.BuildAssetBundle(obj, null, targetPath, BuildAssetBundleOptions.CollectDependencies))
+            {
+                Debug.Log(obj.name + " 资源打包成功");
+            }
+            else
+            {
+                Debug.Log(obj.name + " 资源打包失败");
+            }
+        }
+
+        //Debug.Log("Here");
+
+        AssetDatabase.Refresh();
+    }
+}
---
+++ TestOldAB/Assets/_scripts/OldAssetBundleLoad.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldAssetBundleLoad : MonoBehaviour 
+{
+    public static int m_frameCount = 0;
+
+    // 不同平台下StreamingAssets的路径不同
+    public static readonly string PathURL =
+#if UNITY_ANDROID
+    "jar:file://" + Application.dataPath + "!/assets/";
+
+#elif UNITY_IPHONE
+    Application.dataPath + "/Raw/"
+#elif UNITY_STANDALONE_WIN || UNITY_EDITOR
+    "file://" + Application.dataPath + "/StreamingAssets/";
+#else
+        string.Empty;
+#endif
+
+
+
+    // Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(LoadALLGameObject(PathURL + "ALL.assetbundle"));
+	}
+
+    void Update()
+    {
+        m_frameCount++;
+    }
+
+    private IEnumerator LoadALLGameObject(string path)
+    {
+        // 打印出当前执行的帧数.
+
+        Debug.Log("m_frameCount = "  +m_frameCount);
+
+        WWW bundle = new WWW(path);
+
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+        yield return bundle;
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+        Object obj0 = bundle.assetBundle.Load("Cube1");
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+        Object obj1 = bundle.assetBundle.Load("Cube2");
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+        yield return Instantiate(obj0);
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+
+        yield return Instantiate(obj1);
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+
+        bundle.assetBundle.Unload(false);
+
+        Debug.Log("m_frameCount = " + m_frameCount);
+    }
+}
