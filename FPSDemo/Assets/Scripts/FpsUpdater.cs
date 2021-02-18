a64b7401c95301c5551fad6f464a820b8b10ed38
YuLiang <109827298@qq.com>
2021-02-18 11:29:06 +0800

    添加FPSDemo项目

---
+++ FPSDemo/Assets/Scripts/FpsUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsUpdater : MonoBehaviour
+{
+
+    public Text fpsText;
+
+    private int count;
+    private float deltaTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        count++;
+        deltaTime += Time.deltaTime;
+
+        // 没过0.5s更新一次.
+        if (deltaTime >= 0.5f)
+        {
+            // 经过的帧数 / 经过的时间
+            var fps = count / deltaTime;
+
+            count = 0;
+            deltaTime = 0.0f;
+            fpsText.text = $"FPS: {Mathf.Ceil(fps)}";
+        }
+
+    }
+}
