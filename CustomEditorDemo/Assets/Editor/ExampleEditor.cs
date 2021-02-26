60eb062a8b3ee7daf20803b75f45e6f3995b8f03
YuLiang <109827298@qq.com>
2021-02-26 10:40:50 +0800

    添加CustomEditorDemo

---
+++ CustomEditorDemo/Assets/Editor/ExampleEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Example))]
+public class ExampleEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // 这个target从哪里来
+        Example _example = target as Example;
+
+        if (GUILayout.Button("执行Example方法"))
+        {
+            _example.LogError();
+        }
+    }
+}
---
+++ CustomEditorDemo/Assets/_Scripts/Example.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public void LogError()
+    {
+        Debug.LogError("Test 执行Example方法");
+    }
+}
