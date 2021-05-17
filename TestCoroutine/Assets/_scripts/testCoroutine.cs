2ae35e19745aa8136492ddfcfa804c0a17abb16a
YuLiang <109827298@qq.com>
2021-05-17 14:19:00 +0800

    add files

---
+++ TestCoroutine/Assets/_scripts/testCoroutine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testCoroutine : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("testCoroutine Start.");
+    }
+
+    private IEnumerator TestEnumerator()
+    {
+        Debug.Log("wait for 1s");
+        yield return new WaitForSeconds(1f);
+
+        Debug.Log("wait for 2s");
+        yield return new WaitForSeconds(2f);
+
+        Debug.Log("wait for 3s");
+        yield return new WaitForSeconds(3f);
+
+    }
+}
