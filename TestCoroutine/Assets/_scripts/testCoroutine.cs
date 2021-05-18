8b63b8e465ec3dd55243ac7535339a1e39152609
YuLiang <109827298@qq.com>
2021-05-18 09:32:14 +0800

    add testCoroutine.cs

--- TestCoroutine/Assets/_scripts/testCoroutine.cs
+++ TestCoroutine/Assets/_scripts/testCoroutine.cs
@@ -14,25 +14,15 @@ public class testCoroutine : MonoBehaviour
 
     private IEnumerator Test()
     {
-        // Debug.Log("wait for 1s");
-        // yield return new WaitForSeconds(1f);
-        //
-        // Debug.Log("wait for 2s");
-        // yield return new WaitForSeconds(2f);
-        //
-        // Debug.Log("wait for 3s");
-        // yield return new WaitForSeconds(3f);
-
         int v = 5;
         Debug.Log(v);
-
         yield return new WaitForSeconds(1);
-
         v++;
         Debug.Log(v);
-
         yield return 1;
 
+        // public class ResourceRequest : AsyncOperation
+        ResourceRequest rr = Resources.LoadAsync("test");
 
     }
 }
